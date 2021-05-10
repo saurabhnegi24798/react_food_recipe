764fd36e8c6a45322147e3e3df1d8e5a2d65ffb0
saurabhnegi24798 <saurabhnegi24798@gmail.com>
2021-05-10 18:06:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import Recipie from './Components/Recipie';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    margin: '10px auto',
+    alignItems: 'center',
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+  const [food, setFood] = useState('cake');
+  const [recipies, setRecipies] = useState([]);
+  const [query, setQuery] = useState('');
+
+  const APP_ID = "c2c05ffe", APP_KEY = "353dc6ddbaf37029ab769848aeb49d5d";
+
+  useEffect(() => {
+    console.log('something happened');
+    getRecipie();
+  }, [query]);
+
+  const getRecipie = async () => {
+    const response = await axios.get(`https://api.edamam.com/search?q=${food}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    console.log(response.data.hits);
+    setRecipies(response.data.hits);
+  }
+
+  const updateFood = (event) => {
+    setFood(event.target.value);
+  }
+
+  const updateQuery = (event) => {
+    event.preventDefault();
+    setQuery(food);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Paper component="form" className={classes.root} onSubmit={updateQuery}>
+        <InputBase
+          value={food}
+          onChange={updateFood}
+          className={classes.input}
+          placeholder="Search Recipie"
+          inputProps={{ 'aria-label': 'search recipie' }}
+        />
+        <IconButton type="submit" className={classes.iconButton} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      <Grid container>
+        {
+          recipies.map((recipie) => (
+            <Grid item xs={3}>
+              <Recipie title={recipie.recipe.label} calories={recipie.recipe.calories}
+                image={recipie.recipe.image} ingredients={recipie.recipe.ingredients} />
+            </Grid>
+          ))
+        }
+      </Grid>
     </div>
   );
 }
---
+++ src/Components/Recipie.js
@@ -0,0 +1,83 @@
+import react, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: 20,
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  }
+}));
+
+const Recipie = ({ title, calories, image, ingredients }) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardHeader
+          title={title}
+          subheader={calories}
+        />
+        <CardMedia
+          className={classes.media}
+          image={image}
+          title={title}
+        />
+        <CardContent>
+        </CardContent>
+        <CardActions disableSpacing>
+          <div>Ingredients</div>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            {
+              ingredients.map((ingredient) => {
+                return <Typography paragraph>{ingredient.text}</Typography>;
+              })
+            }
+          </CardContent>
+        </Collapse>
+      </Card>
+    </div>
+  )
+}
+
+export default Recipie;
